c85730f83a3c260ff975d238b530f4b221414a22
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2017-04-09 05:55:17 +0900

    update script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/
+
+for f in `find -mindepth 1 -prune -type f -name '.*'`;
+do
+    echo ${f}
+    HERE=`pwd`
+    if [ -e ~/${f} ]; then
+        if [[ -L ~/${f} ]]; then
+            echo "found a symbolic link"
+            # do nothing if it is symbolic
+        else
+            echo "file ${f} exists; append the content of ${f}"
+            echo "## AUTOMATICALLY APPENDED BY SCRIPT: (FROM MY DOTFILES)" >> ~/${f}
+            cat ${f} >> ~/${f}
+        fi
+    else
+        ln -sfn ${HERE}/${f} ~/${f}
+        echo "created symbolic link ${f}"
+    fi
+done
