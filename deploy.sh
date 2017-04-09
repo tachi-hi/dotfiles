0c0ed0ecc99a8ba79ceb8185307581a578a7ec22
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2017-04-10 05:12:46 +0900

    update deploy script (script to remove old content of dotfiles)

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,13 @@ do
             # do nothing if it is symbolic
         else
             echo "file ${f} exists; append the content of ${f}"
-            cp ~/${f} ~/${f}.bak
+            sed -n '/## AUTOMATICALLY APPENDED BY SCRIPT: (FROM MY DOTFILES)/q;p' ~/${f} > ~/${f}.bak
+            cp ~/${f}.bak ~/${f}
             echo "## AUTOMATICALLY APPENDED BY SCRIPT: (FROM MY DOTFILES)" >> ~/${f}
+            echo "#######################################################" >> ~/${f}
+            echo "## CAUTION!!! DO NOT MANUALLY EDIT ANYTHING AFTER THIS LINE (MAY BE DELETED AUTOMATICALLY BY A SCRIPT)" >> ~/${f}
             cat ${f} >> ~/${f}
+            echo "## CAUTION!!! DO NOT APPEND ANYTHING AFTER THIS LINE (MAY BE DELETED AUTOMATICALLY BY A SCRIPT)" >> ~/${f}
         fi
     else
         ln -sfn ${HERE}/${f} ~/${f}
